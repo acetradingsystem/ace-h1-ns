976f87e6f4e510d8aac21c4d84334b7b4e9df53b
acetradingsystem <miltongaguilar@gmail.com>
2026-05-25 21:42:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -489,6 +489,7 @@ else:
         CLICK RUN SCAN TO START<br><br>
         Scans 640+ TSX stocks on the 1-Hour timeframe<br>
         Detects when MA20 ≈ MA200 within 3% (Narrow State)<br>
+       <br>Detects when MA20 ≈ MA200 within 3% (Narrow State)<br>
        <br>
         Best run at 10:00am ET on trading days
     </div>""", unsafe_allow_html=True)
